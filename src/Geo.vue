9b63b2fa76836eea666375f1a63bebf299504531
David Marr <david.marr@contextlabs.com>
2023-11-01 10:55:22 -0400

    Add maplibre/deck.gl example

--- src/Geo.vue
+++ src/Geo.vue
@@ -1,17 +1,98 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { ScatterplotLayer } from "@deck.gl/layers";
+import { MapboxLayer } from "@deck.gl/mapbox";
+import { onMounted } from "vue";
+
+const maptilerToken = import.meta.env.VITE_MAPTILER_TOKEN;
+
+function addDeckLayers(map) {
+  const layers = map.getStyle().layers;
+
+  let labelLayerId;
+  for (let i = 0; i < layers.length; i++) {
+    if (layers[i].type === "symbol" && layers[i].layout["text-field"]) {
+      labelLayerId = layers[i].id;
+      break;
+    }
+  }
+
+  map.addLayer(
+    {
+      id: "3d-buildings",
+      source: "openmaptiles",
+      "source-layer": "building",
+      // filter: ["==", "extrude", "true"],
+      type: "fill-extrusion",
+      minzoom: 15,
+      paint: {
+        "fill-extrusion-color": "#dca",
+
+        // use an 'interpolate' expression to add a smooth transition effect to the
+        // buildings as the user zooms in
+        "fill-extrusion-height": [
+          "interpolate",
+          ["linear"],
+          ["zoom"],
+          15,
+          0,
+          15.05,
+          ["get", "render_height"],
+        ],
+        "fill-extrusion-base": [
+          "interpolate",
+          ["linear"],
+          ["zoom"],
+          15,
+          0,
+          15.05,
+          ["get", "render_min_height"],
+        ],
+        "fill-extrusion-opacity": 0.6,
+      },
+    },
+    labelLayerId
+  );
+
+  map.addLayer(
+    new MapboxLayer({
+      id: "deckgl-circle",
+      type: ScatterplotLayer,
+      data: [{ position: [ -71.05754, 42.35219], color: [255, 0, 0], radius: 200 }],
+      getPosition: (d) => d.position,
+      getFillColor: (d) => d.color,
+      getRadius: (d) => d.radius,
+      opacity: 0.3,
+    }),
+    labelLayerId
+  );
+}
+
+function addDeckSources(map) {
+  map.addSource('openmaptiles', {
+      type: 'vector',
+      url: 'https://api.maptiler.com/tiles/v3-openmaptiles/tiles.json?key=PT04u2C5qXTCxBPacrE4'
+  });
+}
 
 onMounted(() => {
   const map = new maplibregl.Map({
-    container: 'map',
-    // style: 'https://demotiles.maplibre.org/style.json'
-    style: 'map.style.json',
+    antialias: true,
+    container: "map",
+    bearing: -127.5443,
+    center: [-71.05836438462632, 42.35671247471868],
+    pitch: 60,
+    zoom: 15.345,
+    style:
+      `https://api.maptiler.com/maps/dataviz/style.json?key=${maptilerToken}`
+      // `https://api.maptiler.com/maps/basic-v2/style.json?key=${maptilerToken}`
+      // `https://api.maptiler.com/maps/streets/style.json?key=${maptilerToken}`,
+      // `https://api.maptiler.com/maps/backdrop/style.json?key=${maptilerToken}`,
+      // `https://demotiles.maplibre.org/style.json`
+      // "map.style.json"
   });
-  map.on("idle", () => {
-    // controls.value.bearing = mb.getBearing();
-    // controls.value.zoom = mb.getZoom();
-    // controls.value.center = mb.getCenter();
-    // controls.value.pitch = mb.getPitch();
+  map.on("load", () => {
+    addDeckSources(map);
+    addDeckLayers(map);
   });
 });
 </script>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        // nested: resolve(__dirname, 'nested/index.html'),
+      },
+    },
+  },
   plugins: [vue()],
 })
